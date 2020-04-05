584ee0b0bfb3bca9204b44a9e6943ce77ddeb2df
MaryEtak <marykatewhite@gmail.com>
2020-04-04 21:25:56 -0400

    a footer.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-function Footer(props) {
-    // return ;
+function Footer() {
+  return (
+    <nav className="navbar fixed-bottom navbar-light bg-light">
+        This is a footer.
+    </nav>
+  );
 }
 
 export default Footer;
