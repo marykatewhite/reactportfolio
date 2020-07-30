cb5d571870e93ced6c258188e28a7e570edc449e
MaryEtak <marykatewhite@gmail.com>
2020-07-30 17:29:54 -0400

    tweaks mostly to css

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Content from "./components/content.js";
 // import Jumbotron from "./components/jumbotron.js";
 import Contact from "./components/contact.js"
 import Resume from "./components/resume.js";
-import Design from "./components/design.js";
-// import DesignPage from "./components/designpage.js";
+// import Design from "./components/design.js";
+import DesignPage from "./components/designpage.js";
 import Writing from "./components/writing.js";
 import projects from "./projects.json";
 import "./App.css";
@@ -25,7 +25,7 @@ class App extends Component {
           <Header />
           <Route exact path="/" component={Content} />
           <Route exact path="/code" component={Project} />
-          <Route exact path="/design" component={Design} />
+          <Route exact path="/design" component={DesignPage} />
           <Route exact path="/writing" component={Writing} />
           <Route exact path="/resume" component={Resume} />
           <Route exact path="/contact" component={Contact} />
--- src/components/content.js
+++ src/components/content.js
@@ -4,52 +4,68 @@ import "../App.css";
 function Content() {
   return (
     <div className="jumbotron-fluid" id="contentTypeBox">
-      <div className="container" id="contentTypes">
-
-            <div className="row">
-
-              <div className="col xs12 m4 justify-content-center">
-                <a href="/projects">
-              <div className="contentTitleBox justify-content-center">
-                <h2 className="contentTitle">
-                  Programming
-                  <br />
-                  &amp; Code
-                </h2>
-                </div>
-                <div className="contentImgBox" id="programmingBox"></div></a>
-              </div>
-
-              <div className="col xs12 m4 justify-content-center">
-              <a href="/design">
-                <div className="contentTitleBox justify-content-center">
-                <h2 className="contentTitle">
-                  Design
-                  <br />
-                  &amp; Layout
-                </h2>
-                </div>
-                <div className="contentImgBox" id="designBox"></div></a>
-              </div>
-
-              <div className="col xs12 m4 justify-content-center">
-              <a href="/writing">
-              <div className="contentTitleBox justify-content-center">
-                <h2 className="contentTitle">
-                  Writing
-                  <br />
-                  &amp; Editing
-                </h2>
-                </div>
-                <div className="contentImgBox" id="writingBox"></div></a>
-              </div>
-
-            </div>
-
-            
+      <div className="container-fluid" id="namebox">
+        <div className="row justify-content-center">
+          <div className="col-xs-12">
+            <h1 id="myName">Mary Kate White</h1>
           </div>
         </div>
 
+        <div id="bioBox">
+          <div className="row justify-content-center">
+            <div className="col-xs-12">
+              <p id="introText">
+                An <a href="/writing"><b>editor</b></a>,{" "}<a href="/design"><b>designer</b></a> and{" "}
+                <a href="/code"><b>full-stack web developer.</b></a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container-fluid" id="contentTypes">
+        <div className="row">
+          <div className="col xs12 m4 justify-content-center contentColumn">
+            <a href="/projects">
+              <div className="contentTitleBox justify-content-center">
+                <p className="contentTitle">
+                  Programming
+                  <br />
+                  &amp; Code
+                </p>
+              </div>
+              <div className="contentImgBox" id="programmingBox"></div>
+            </a>
+          </div>
+
+          <div className="col xs12 m4 justify-content-center">
+            <a href="/design">
+              <div className="contentTitleBox justify-content-center">
+                <p className="contentTitle">
+                  Design
+                  <br />
+                  &amp; Layout
+                </p>
+              </div>
+              <div className="contentImgBox" id="designBox"></div>
+            </a>
+          </div>
+
+          <div className="col xs12 m4 justify-content-center">
+            <a href="/writing">
+              <div className="contentTitleBox justify-content-center">
+                <p className="contentTitle">
+                  Writing
+                  <br />
+                  &amp; Editing
+                </p>
+              </div>
+              <div className="contentImgBox" id="writingBox"></div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
