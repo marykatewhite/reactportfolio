6e761e8cdc758bb509b2865f1a0a58685d1b819a
MaryEtak <marykatewhite@gmail.com>
2020-07-30 17:01:17 -0400

    fixes imports, ruins some css

--- src/App.js
+++ src/App.js
@@ -6,13 +6,12 @@ import Content from "./components/content.js";
 // import Jumbotron from "./components/jumbotron.js";
 import Contact from "./components/contact.js"
 import Resume from "./components/resume.js";
-// import Design from "./components/design.js";
-import DesignPage from "./components/designpage.js";
+import Design from "./components/design.js";
+// import DesignPage from "./components/designpage.js";
 import Writing from "./components/writing.js";
 import projects from "./projects.json";
-import 'materialize-css/dist/css/materialize.min.css';
 import "./App.css";
-import Design from "./components/design.js";
+import 'materialize-css/dist/css/materialize.min.css';
 
 class App extends Component {
   state = {
