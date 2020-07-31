5f14a979d599d544015401d682342d53566bd314
MaryEtak <marykatewhite@gmail.com>
2020-07-30 21:52:14 -0400

    idk anymore

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/header.js";
-import Project from "./components/project.js";
-import Content from "./components/content.js";
-// import Jumbotron from "./components/jumbotron.js";
 import Contact from "./components/contact.js"
 import Resume from "./components/resume.js";
-// import Design from "./components/design.js";
 import DesignPage from "./components/designpage.js";
 import WritingPage from "./components/writingpage";
 import CodePage from "./components/codepage";
-// import Writing from "./components/writing.js";
 import projects from "./projects.json";
 import "./App.css";
 import 'materialize-css/dist/css/materialize.min.css';
